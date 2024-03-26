9e03c4c41a354c6d101c9cb02bd2a939ff9e54fe
Nadia <161623420+Nadia467@users.noreply.github.com>
2024-03-26 23:56:00 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "enter first number", type: "number", name: "firstnumber" },
+    { message: "enter second number", type: "number", name: "secondnumber" },
+    {
+        message: "select one of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["addition", "multipication", "subraction", "division"],
+    },
+]);
+console.log(answer);
+//conditional statement
+if (answer.operator === "addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "subraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "multipication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    console.log("please select valit operator");
+}
